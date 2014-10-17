bc7a1a976b452fbbc1d43ac24e173d29fe3e68e3
erinspace <erin.braswell@gmail.com>
2014-10-17 10:48:33 -0400

    added some functionality

--- scrapi_stats.py
+++ scrapi_stats.py
@@ -18,14 +18,14 @@ DEFAULT_PARAMS = {
     'sort_type': 'desc',
     'from': 0,
     'size': 10,
-    'format': 'json'
+    'format': 'json',
+    'empty_field': None
 }
 
 
 def query_osf(query):
     headers = {'Content-Type': 'application/json'}
     data = json.dumps(query)
-#     import pdb; pdb.set_trace()
     print(data)
     return requests.post(OSF_APP_URL, auth=OSF_AUTH, headers=headers, data=data, verify=False).json()
 
@@ -44,13 +44,13 @@ def search(raw_params):
     return query_osf(query)
 
 
-
 def parse_query(params):
     return {
         'query': build_query(
             params.get('q'),
             params.get('start_date'),
-            params.get('end_date')
+            params.get('end_date'),
+            params.get('empty_field')
         ),
         'sort': build_sort(params.get('sort_field'), params.get('sort_type')),
         'from': params.get('from'),
@@ -58,13 +58,29 @@ def parse_query(params):
     }
 
 
-def build_query(q, start_date, end_date):
-    return {
-        'filtered': {
-             'query': build_query_string(q),
-             'filter': build_date_filter(start_date, end_date),
+def build_query(q, start_date, end_date, empty_field):
+    # import pdb; pdb.set_trace()
+    if empty_field is not None:
+        return {
+            'filtered': {
+                'query': build_query_string(q),
+                'filter': {
+                    'bool': {
+                        'must': [
+                            build_date_filter(start_date, end_date),
+                            build_empty_filter(empty_field)
+                        ]
+                    }
+                }
+            }
         }
-    }
+    else:
+        return {
+            'filtered': {
+                'query': build_query_string(q),
+                'filter': build_date_filter(start_date, end_date)
+            }
+        }
 
 
 def build_query_string(q):
@@ -88,6 +104,15 @@ def build_date_filter(start_date, end_date):
         }
     }
 
+
+def build_empty_filter(empty_field):
+    return {
+        'terms' : {
+            empty_field: ''
+        }
+    }
+
+
 def build_sort(sort_field, sort_type):
     print sort_field
     return [{
@@ -96,7 +121,8 @@ def build_sort(sort_field, sort_type):
         }
     }]
 
-all_results = search({'size': 1000})
+all_results = search({'size': 1000, 'empty_field': 'doi'})
+import pdb; pdb.set_trace()
 
 
 
