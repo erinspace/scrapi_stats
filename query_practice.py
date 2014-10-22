ab301724e6718b0f75c66f944e6794ccf4572f1d
erinspace <erin.braswell@gmail.com>
2014-10-22 14:18:12 -0400

    added request params and query params

--- query_practice.py
+++ query_practice.py
@@ -19,35 +19,38 @@ DEFAULT_PARAMS = {
     'agg': False
 }
 
+DEFAULT_REQUEST_PARAMS = {
+    'return_raw': False
+}
+
 # Local settings
-OSF_APP_URL = 'http://localhost:5000/api/v1/app/kb7ae/agg/'
+OSF_APP_URL = 'http://localhost:5000/api/v1/app/kb7ae/?return_raw=True'
 OSF_AUTH = ('scrapi', '54331d5a669aec8577089ad1e4b9dcb3-50f3-4828-9232-d85512a999e7')
 
 # share-dev settings
 # OSF_APP_URL = 'https://share-dev.osf.io/api/v1/app/6qajn/'
 # OSF_AUTH = ('scrapi_stats','543edf86b5e9d7579327c710eb1d94ee-d8da-472a-84bb-ba6b96499c80')
 
-def query_osf(query):
+def query_osf(query, raw_query_params, request_params):
     headers = {'Content-Type': 'application/json'}
     data = json.dumps(query, indent=4)
     print(data)
+    request_url_params = ['{}={}'.format(key,str(value)) for key, value in request_params.iteritems()]
+    request_string = ''
+    for param in request_url_params:
+        request_string += param
+    request_param_string = OSF_APP_URL + request_string
+    # import pdb; pdb.set_trace()
     return requests.post(OSF_APP_URL, auth=OSF_AUTH, headers=headers, data=data, verify=False).json()
 
-
-def search(raw_params):
+def search(request_params, raw_query_params):
     params = copy.deepcopy(DEFAULT_PARAMS)
-    params.update(raw_params)
+    params.update(raw_query_params)
     for key in params.keys():
         if isinstance(params[key], list) and len(params[key]) == 1:
             params[key] = params[key][0]
-    params['from'] = int(params['from'])
-    params['size'] = int(params['size'])
-    # print(params)
-    # query = parse_query(params)
     query = create_test_agg_query()
-    query['format'] = params.get('format')
-    osf_query = query_osf(query)
-    # import pdb; pdb.set_trace()
+    osf_query = query_osf(query, raw_query_params, request_params)
     return osf_query
 
 
@@ -55,10 +58,11 @@ def search(raw_params):
 def create_test_agg_query():
     return {
         "size" : 0,
+        # "return_raw": True,
         "aggs": {
             "sourceAggregation": {
                 "filter" : {
-                    "missing" : {"field" : "title"}
+                    "missing" : {"field" : "prefix"}
                 },
 
                 "aggs" : {
@@ -72,9 +76,7 @@ def create_test_agg_query():
     }
 
 
-search_osf = search({})
+search_osf = search(request_params={'return_raw': True}, raw_query_params={})
 
 print(json.dumps(search_osf, indent=4))
-
-# import pdb; pdb.set_trace()
 
