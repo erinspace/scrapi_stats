ed237edb608ff7e976018d6e865af3350214caf9
erinspace <erin.braswell@gmail.com>
2014-10-23 12:19:12 -0400

    new notebook using aggregations - makes things a whole lot simpler

--- query_practice.py
+++ query_practice.py
@@ -31,29 +31,16 @@ DEFAULT_REQUEST_PARAMS = {
 OSF_APP_URL = 'https://share-dev.osf.io/api/v1/app/6qajn/?return_raw=True'
 OSF_AUTH = ('scrapi_stats','543edf86b5e9d7579327c710eb1d94ee-d8da-472a-84bb-ba6b96499c80')
 
-def query_osf(query, raw_query_params, request_params):
+def query_osf(query):
     headers = {'Content-Type': 'application/json'}
-    data = json.dumps(query, indent=4)
-    print(data)
-    request_url_params = ['{}={}'.format(key,str(value)) for key, value in request_params.iteritems()]
-    request_string = ''
-    for param in request_url_params:
-        request_string += param
-    request_param_string = OSF_APP_URL + request_string
-    # import pdb; pdb.set_trace()
     return requests.post(OSF_APP_URL, auth=OSF_AUTH, headers=headers, data=data, verify=False).json()
 
 def search(request_params, raw_query_params):
-    params = copy.deepcopy(DEFAULT_PARAMS)
-    params.update(raw_query_params)
-    for key in params.keys():
-        if isinstance(params[key], list) and len(params[key]) == 1:
-            params[key] = params[key][0]
-    query = create_test_nested_agg_query()
-    osf_query = query_osf(query, raw_query_params, request_params)
+    query = nested_agg_query()
+    osf_query = query_osf(query)
     return osf_query
 
-def create_simple_agg_query():
+def simple_agg_query():
     return {
         "size" : 0,
         "aggs": {
@@ -63,7 +50,7 @@ def create_simple_agg_query():
         }
     }
 
-def create_test_nested_agg_query():
+def nested_agg_query():
     return {
         "size" : 0,
         "aggs": {
