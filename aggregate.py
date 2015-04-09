80b8375da4d54b01b4114b8c53e43df59211314a
erinspace <erin.braswell@gmail.com>
2015-04-09 11:18:02 -0400

    Add include query and terms for most popular terms

--- aggregate.py
+++ aggregate.py
@@ -1,38 +1,14 @@
 from __future__ import division
 
-import sys
 import json
 import argparse
-import datetime
 import requests
-
-import getopt
-
-
-DEFAULT_PARAMS = {
-    'q': '*',
-    'start_date': None,
-    'end_date': datetime.date.today().isoformat(),
-    'sort_field': 'dateUpdated',
-    'sort_type': 'desc',
-    'from': 0,
-    'size': 10,
-    'format': 'json',
-    'empty_field': None,
-    'agg': False
-}
-
-DEFAULT_REQUEST_PARAMS = {
-    'return_raw': False
-}
 
 # Local settings
 OSF_APP_URL = 'http://localhost:5000/api/v1/share/search/?raw=True'
-# OSF_AUTH = ('scrapi', '54331d5a669aec8577089ad1e4b9dcb3-50f3-4828-9232-d85512a999e7')
 
 # share-dev settings
-# OSF_APP_URL = 'https://share-dev.osf.io/api/v1/app/6qajn/?return_raw=True'
-# OSF_AUTH = ('scrapi_stats','543edf86b5e9d7579327c710eb1d94ee-d8da-472a-84bb-ba6b96499c80')
+# OSF_APP_URL = 'https://osf.io/api/v1/share/search/?raw=True'
 
 
 def query_osf(query):
@@ -49,20 +25,9 @@ def search(aggs):
     return osf_query
 
 
-def simple_agg_query():
-    return {
-        "size": 0,
-        "aggs": {
-            "sources": {
-                "terms": {"field": "source"}
-            }
-        }
-    }
-
-
 def missing_agg_query(terms):
     return {
-        "missing{}Aggregation".format(term): {
+        "{}MissingAggregation".format(term): {
             "filter": {
                 "missing": {"field": term}
             },
@@ -76,10 +41,43 @@ def missing_agg_query(terms):
     }
 
 
+def includes_agg_query(terms):
+    return {
+        "{}NotMissingAggregation".format(term): {
+            "filter": {
+                "not": {
+                    "missing": {"field": term}
+                }
+            },
+
+            "aggs": {
+                "sources": {
+                    "terms": {"field": "source"}
+                }
+            }
+        } for term in terms
+    }
+
+
+def terms_agg_query(terms, size):
+    return {
+        "{}TermFilter".format(term): {
+            "terms": {
+                "field": term,
+                "size": size,
+                "exclude": "of|and|or"
+            }
+        } for term in terms
+    }
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="A command line interface for getting numbers of SHARE sources missing given terms")
 
-    parser.add_argument('-m', '--missing', dest='missing', type=str, help='The terms to aggregate with.', nargs='+')
+    parser.add_argument('-m', '--missing', dest='missing', type=str, help='The terms to aggregate with, to find sources with terms missing', nargs='+')
+    parser.add_argument('-t', '--terms', dest='terms', type=str, help='The top unique entries with given terms. Use with size to control number of results shown.', nargs='+')
+    parser.add_argument('-s', '--size', dest='size', type=int, help='The number of results to return per aggretation', default=0)
+    parser.add_argument('-i', '--includes', dest='includes', type=str, help='The terms to aggregate with, to find sources with terms included', nargs='+')
 
     return parser.parse_args()
 
@@ -89,8 +87,10 @@ def main():
     aggs = {}
     if args.missing:
         aggs.update(missing_agg_query(args.missing))
-    # if args.whatever:
-    #     aggs.update(other_function(args.whatever))
+    if args.terms:
+        aggs.update(terms_agg_query(args.terms, args.size))
+    if args.includes:
+        aggs.update(includes_agg_query(args.includes))
 
     search_osf = search(aggs)
 
